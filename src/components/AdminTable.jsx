790654ce26416a65aeb8c3574421d5fffd440ba2
AzamatRaimbekov <arturfunks88@gmail.com>
2022-04-26 13:08:23 +0300

    Azamat all Crud AdminPage

--- src/components/AdminTable.jsx
+++ src/components/AdminTable.jsx
@@ -1,7 +1,88 @@
-import React from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Link } from "react-router-dom";
+import { Button } from "@mui/material";
+import { admimContext, adminContext } from "../contexts/AdminContext";
 
-const AdminTable = () => {
-  return <div>AdminSide</div>;
-};
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
 
-export default AdminTable;
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+export default function CustomizedTables(props) {
+  const data = React.useContext(adminContext);
+  const { deleteProduct } = data;
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Вид</StyledTableCell>
+            <StyledTableCell align="right">Картинка</StyledTableCell>
+            <StyledTableCell align="right">Название</StyledTableCell>
+            <StyledTableCell align="right">цена</StyledTableCell>
+            <StyledTableCell align="right">память</StyledTableCell>
+            <StyledTableCell align="right">память озу</StyledTableCell>
+            <StyledTableCell align="right">#</StyledTableCell>
+            <StyledTableCell align="right">#</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.products.map((item, index) => (
+            <StyledTableRow key={item.id}>
+              <StyledTableCell component="th" scope="row">
+                {index + 1}
+              </StyledTableCell>
+              <StyledTableCell align="right">{item.name}</StyledTableCell>
+              <StyledTableCell align="right">
+                {item.description}
+              </StyledTableCell>
+              <StyledTableCell align="right">{item.price}</StyledTableCell>
+              <StyledTableCell align="right">
+                <img width={100} src={item.image} />
+              </StyledTableCell>
+              <StyledTableCell align="right">{item.ram}</StyledTableCell>
+              <StyledTableCell align="right">{item.memory}</StyledTableCell>
+              <StyledTableCell align="right">{item.color}</StyledTableCell>
+              <StyledTableCell align="right">{item.memory}</StyledTableCell>
+
+              <StyledTableCell align="right">
+                <Link to={`/admin-panel-edit/${item.id}`}>EDIT</Link>
+              </StyledTableCell>
+
+              <StyledTableCell align="right">
+                <Button onClick={() => deleteProduct(item.id)}>Del</Button>
+              </StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/pages/EditProductPage.jsx
@@ -0,0 +1,166 @@
+import {
+  Button,
+  Container,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { adminContext } from "../contexts/AdminContext";
+import { useNavigate, useParams } from "react-router-dom";
+const EditProductPage = () => {
+  const data = React.useContext(adminContext);
+  const { getProductsToEdit, productToEdit, saveEditedPruduct } = data;
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const [editedProduct, setEditedProduct] = useState(productToEdit);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    for (let key in editedProduct) {
+      let value = editedProduct[key];
+      if (typeof value === "string") {
+        if (!value.trim()) {
+          alert("Заполните поля");
+          return;
+        }
+      }
+    }
+    saveEditedPruduct(editedProduct);
+    navigate("/admim-panel");
+  };
+
+  useEffect(() => {
+    getProductsToEdit(params.id);
+  }, []);
+
+  useEffect(() => {
+    setEditedProduct(productToEdit);
+  }, [productToEdit]);
+
+  if (!editedProduct) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <Container>
+      <div className="add-edit-page">
+        <h2>Добавить товар</h2>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            onChange={(e) =>
+              setEditedProduct({ ...editedProduct, color: e.target.value })({
+                ...editedProduct,
+                name: e.target.value,
+              })
+            }
+            value={editedProduct.name}
+            label="Введите название"
+            variant="standard"
+          />
+          <TextField
+            onChange={(e) =>
+              setEditedProduct({ ...editedProduct, color: e.target.value })({
+                ...editedProduct,
+                description: e.target.value,
+              })
+            }
+            value={editedProduct.description}
+            label="Введите описание"
+            variant="standard"
+          />
+          <TextField
+            type="number"
+            onChange={(e) =>
+              setEditedProduct({ ...editedProduct, color: e.target.value })({
+                ...editedProduct,
+                price: parseInt(e.target.value),
+              })
+            }
+            value={editedProduct.price}
+            label="Введите цену"
+            variant="standard"
+          />
+          <TextField
+            onChange={(e) =>
+              setEditedProduct({ ...editedProduct, image: e.target.value })
+            }
+            value={editedProduct.image}
+            label="Введите фото"
+            variant="standard"
+          />
+          <FormControl variant="standard">
+            <InputLabel id="color-select-label">Выберите цвет</InputLabel>
+            <Select
+              onChange={(e) =>
+                setEditedProduct({ ...editedProduct, color: e.target.value })({
+                  ...editedProduct,
+                  color: e.target.value,
+                })
+              }
+              value={editedProduct.color}
+              label="Выберите цвет"
+              labelId="color-select-label"
+            >
+              <MenuItem value="black">Чёрный</MenuItem>
+              <MenuItem value="white">Белый</MenuItem>
+              <MenuItem value="blue">Синий</MenuItem>
+              <MenuItem value="pink">Розовый</MenuItem>
+              <MenuItem value="yellow">Жёлтый</MenuItem>
+              <MenuItem value="red">Красный</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl variant="standard">
+            <InputLabel id="memory-select-label">Выберите память</InputLabel>
+            <Select
+              onChange={(e) =>
+                setEditedProduct({ ...editedProduct, color: e.target.value })({
+                  ...editedProduct,
+                  memory: e.target.value,
+                })
+              }
+              value={editedProduct.memory}
+              label="Выберите память"
+              labelId="memory-select-label"
+            >
+              <MenuItem value="256">256 GB</MenuItem>
+              <MenuItem value="512">512 GB</MenuItem>
+              <MenuItem value="1024">1024 GB</MenuItem>
+              <MenuItem value="2048">2048 GB</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl variant="standard">
+            <InputLabel id="ram-select-label">Выберите Озу</InputLabel>
+            <Select
+              onChange={(e) =>
+                setEditedProduct({ ...editedProduct, color: e.target.value })({
+                  ...editedProduct,
+                  ram: e.target.value,
+                })
+              }
+              value={editedProduct.ram}
+              label="Выберите ОЗУ "
+              labelId="ram-select-label"
+            >
+              <MenuItem value="4">4 GB</MenuItem>
+              <MenuItem value="8">8 GB</MenuItem>
+              <MenuItem value="16">16 GB</MenuItem>
+              <MenuItem value="32">32 GB</MenuItem>
+              <MenuItem value="64">64 GB</MenuItem>
+            </Select>
+          </FormControl>
+          <Button type="submit" variant="outlined">
+            Добавить
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+};
+
+export default EditProductPage;
